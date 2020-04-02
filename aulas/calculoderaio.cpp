84491de1e97d169a118aa511abbc068b6cfcce00
Rafael Aranha <57453206+rafaolidev@users.noreply.github.com>
2020-04-02 19:15:35 -0300

    Create calculoderaio.cpp

---
+++ aulas/calculoderaio.cpp
@@ -0,0 +1,16 @@
+// Example program
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    float b,h,A;
+    cout<<"digite o valor da base (b)"<<endl;
+    cin>>b;
+    cout<<"digite o valor da base (h)"<<endl;
+    cin>>h;
+    
+    A = (b*h)/2;
+    cout<<"O resultado da area e de "<<A<<" milimetros quadrados";
+    
+}
