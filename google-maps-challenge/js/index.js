7081fa97077f1b3dc3345803631ed4c89079f7f0
Rafael Aranha <57453206+rafaolidev@users.noreply.github.com>
2020-04-09 15:29:37 -0300

    Add files via upload

--- google-maps-challenge/js/index.js
+++ google-maps-challenge/js/index.js
@@ -1,6 +1,9 @@
 
-
-
+window.onload = function() {
+  displayStores();
+}
+var map;
+var markers = [];
 
 function initMap() {
 
@@ -12,66 +15,158 @@ function initMap() {
       center: losAngeles,
       zoom: 11,
       mapTypeId: 'roadmap',
-    });
-    var manhattan = {
-        lat: 33.885442,
-        lng: -118.411091
-        
-    };
-    var hermosabeach = {
-        lat: 33.860387,
-        lng: -118.400573
-        
-    };
-    var redondobeach = {
-        lat: 33.846439,
-        lng: -118.391473
-        
-    };
-    var burnoutbeach = {
-        lat: 33.815820,
-        lng: -118.390278
-        
-    };
-    var marker = new google.maps.Marker({
-        position: manhattan,
-        icon: {    
-            url: drawMarker("#000")
-          },
-        map: map,
-        title: '209-201 12th St,\n Manhattan Beach,\n CA 90266, EUA'
-      });
-      var marker = new google.maps.Marker({
-        position: hermosabeach,
-        icon: {    
-            url: drawMarker("#000")
-          },
-        map: map,
-        title: 'Hermosa Beach,\n CA 90254,\n EUA'
-      });
-      var marker = new google.maps.Marker({
-        position: redondobeach,
-        icon: {    
-            url: drawMarker("#000")
-          },
-        map: map,
-        title: '1898-1800 Esplanade,\n Redondo Beach,\n CA 90277, EUA',
-      });
-      var marker = new google.maps.Marker({
-        position: burnoutbeach,
-        icon: {    
-            url: drawMarker("#000")
-          },
-        map: map,
-        title: '1898-1800 Esplanade,\n Redondo Beach,\n CA 90277, EUA'
-      });
+      styles: [
+        {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+        {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+        {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+        {
+          featureType: 'administrative.locality',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#d59563'}]
+        },
+        {
+          featureType: 'poi',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#d59563'}]
+        },
+        {
+          featureType: 'poi.park',
+          elementType: 'geometry',
+          stylers: [{color: '#263c3f'}]
+        },
+        {
+          featureType: 'poi.park',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#6b9a76'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'geometry',
+          stylers: [{color: '#38414e'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#212a37'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#9ca5b3'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry',
+          stylers: [{color: '#746855'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#1f2835'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#f3d19c'}]
+        },
+        {
+          featureType: 'transit',
+          elementType: 'geometry',
+          stylers: [{color: '#2f3948'}]
+        },
+        {
+          featureType: 'transit.station',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#d59563'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'geometry',
+          stylers: [{color: '#17263c'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#515c6d'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'labels.text.stroke',
+          stylers: [{color: '#17263c'}]
+        }
+      ],
       
-      function drawMarker(color){
-        return 'data:image/svg+xml;utf-8, \
-        <svg width="54px" height="54px" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="umbrella-beach" class="svg-inline--fa fa-umbrella-beach fa-w-20" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="currentColor" d="M115.38 136.9l102.11 37.18c35.19-81.54 86.21-144.29 139-173.7-95.88-4.89-188.78 36.96-248.53 111.8-6.69 8.4-2.66 21.05 7.42 24.72zm132.25 48.16l238.48 86.83c35.76-121.38 18.7-231.66-42.63-253.98-7.4-2.7-15.13-4-23.09-4-58.02.01-128.27 69.17-172.76 171.15zM521.48 60.5c6.22 16.3 10.83 34.6 13.2 55.19 5.74 49.89-1.42 108.23-18.95 166.98l102.62 37.36c10.09 3.67 21.31-3.43 21.57-14.17 2.32-95.69-41.91-187.44-118.44-245.36zM560 447.98H321.06L386 269.5l-60.14-21.9-72.9 200.37H16c-8.84 0-16 7.16-16 16.01v32.01C0 504.83 7.16 512 16 512h544c8.84 0 16-7.17 16-16.01v-32.01c0-8.84-7.16-16-16-16z"></path></svg>';
-      }
+    });
+    infoWindow = new google.maps.InfoWindow();
 
+    showStoresMarkers();
+    
   } 
+
 function displayStores(){
-  for(var store of store);
+  var storesHtml ='';
+  for(var [index, store] of stores.entries()){
+    var address = store ['addressLines'];
+    var phone = store ['phoneNumber'];
+    storesHtml += `
+    <div class="store-container">
+                   <div class="store-info-container">
+                          <div class="store-address">
+                                 ${address[0]}<br> ${address[1]}
+                          </div>
+                          <div class="store-phone-number"> ${phone} </div>
+                       </div>
+                   <div class="store-number-container">
+                       <div class="store-number"> 
+                       ${index+1}
+                       </div>
+                   </div>
+                 
+            
+                </div>
+    
+    `
+    document.querySelector('.stores-list').innerHTML = storesHtml;
+    
+    
+  
+  }
+}   
+
+    
+function showStoresMarkers(){
+  var bounds = new google.maps.LatLngBounds();
+  for(var [index, store] of stores.entries()){
+      var latlng = new google.maps.LatLng(
+          store["coordinates"]["latitude"],
+          store["coordinates"]["longitude"]);
+      var name = store["name"];
+      var address = store["addressLines"][0];
+
+      bounds.extend(latlng);
+      createMarker(latlng, name, address, index+1)
+  }
+  map.fitBounds(bounds);
 }
+
+function createMarker(latlng, name, address, index){
+  var icon = {
+      url: "https://upload.wikimedia.org/wikipedia/sco/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/297px-Starbucks_Corporation_Logo_2011.svg.png", // url
+      scaledSize: new google.maps.Size(35, 35)
+    };
+  var marker = new google.maps.Marker({
+    map: map,
+    position: latlng,
+    animation: google.maps.Animation.DROP,
+    icon:icon, 
+  });
+  var html = "<b>" + name + "</b> <br/>" + address;
+  google.maps.event.addListener(marker, 'click', function() {
+    infoWindow.setContent(html);
+    infoWindow.open(map, marker);
+  });
+  markers.push(marker);
+}
+
+
+
